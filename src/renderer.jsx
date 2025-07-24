3c302b6edba5e403e4d300f85f6e823c7393373c
odd601 <2848703468@qq.com>
2025-07-24 15:46:58 +0800

    feat: ✨ui demo

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,15 +1,10 @@
-import Intro from "./components/Intro";
+import Main from "./Main.tsx";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 
-const fullScreenCenterItems =
-  "flex flex-col content-center justify-center items-center w-screen h-screen";
-
 const App = () => {
   return (
-    <div className={fullScreenCenterItems + " bg-amber-100 "}>
-      <Intro />
-    </div>
+    <Main />
   );
 };
 const container = document.getElementById("root");
